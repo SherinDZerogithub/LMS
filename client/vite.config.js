90c707443bb216cdf92c2b7b0037ed9582b47bf9
SherinDZerogithub <seriniharindya@gmail.com>
2025-05-01 20:50:58 +0530

    Deploying to GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/LMS/",
   plugins: [react()],
-  base: "/LMS/", // this must match your GitHub repo name
+   // this must match your GitHub repo name
 });
